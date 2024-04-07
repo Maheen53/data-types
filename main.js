3120574ea03d3d93f6540bd744690785b550d824
Maheen imtiaz <maheenimtiaz536@gmail.com>
2024-04-06 19:47:43 -0700

    data type covered

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// console.log("hello world !");
+//variables
+// let
+let name1 = "variable by let";
+console.log(name1);
+name1 = "maheen";
+console.log(name1);
+// var
+//  var name1 = "variable by var";
+//  name1 =" maheen ";
+//  console.log(name1);
---
+++ main.ts
@@ -0,0 +1,84 @@
+        // console.log("hello world !");
+
+ //variables
+
+ // let
+//   let name1 = "variable by let";
+
+//   console.log(name1);
+
+//   name1 = "maheen";
+
+//   console.log(name1);
+
+
+ // var
+//  var name1 = "variable by var";
+
+//  name1 =" maheen ";
+
+//  console.log(name1);
+
+
+// // const
+
+// const name1 = "variable by const";
+
+// console.log(name1);
+
+
+// // data types
+
+// string
+
+// const name1 = "maheem"; //inferring type
+
+// const name2 : string = "maheen imtiaz" // strongly typee
+
+// // number 
+
+// const num1 = 100
+
+// const num2 : number = 2
+
+// boolean
+
+const news = true;
+
+const gnews : boolean = false
+
+let anyData ="11"
+
+let anyData2 = "maheen"
+
+let anydata3 = "true"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
